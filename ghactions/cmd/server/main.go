e236122c2099b610432c954459ee799a3584e6b6
Jeton Shehu <jeton.shehu@p2p.org>
2025-03-21 15:53:16 +0100

    chore: add version support

--- ghactions/cmd/server/main.go
+++ ghactions/cmd/server/main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"fmt"
+	"gh-actions/internal/version"
 	"time"
 )
 
 func main() {
-	fmt.Println("gh-actions")
+	fmt.Println("gh-actions", version.Version)
 
 	count := 1
 	for {
